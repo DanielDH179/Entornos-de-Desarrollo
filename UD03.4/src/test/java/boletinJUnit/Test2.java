dc41c7afcfe7bb40ad28b12fe41b05142397ec33
DanielDH179 <95979922+DanielDH179@users.noreply.github.com>
2024-02-18 22:29:11 +0100

    Update Test2.java

--- UD03.4/src/test/java/boletinJUnit/Test2.java
+++ UD03.4/src/test/java/boletinJUnit/Test2.java
@@ -3,19 +3,15 @@ package boletinJUnit;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.nullValue;
-import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class Test2 {
