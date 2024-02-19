8191a0ec75574b2df9cb326f6e02485389e86a30
DanielDH179 <95979922+DanielDH179@users.noreply.github.com>
2024-02-19 21:02:16 +0100

    Update Test7.java

--- UD03.4/src/test/java/boletinJUnit/Test7.java
+++ UD03.4/src/test/java/boletinJUnit/Test7.java
@@ -1,3 +1,33 @@
 package boletinJUnit;
 
-public class Test7 {}
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class Test7 {
+	
+	// Sesión de cobertura del 21,2% para Account
+	Account cuenta;
+	
+	@BeforeEach
+	void init() {
+		cuenta = new Account("Juan", 112233445566L, 600F);
+	}
+	
+	@AfterEach
+	void finish() {
+		cuenta = null;
+	}
+
+    @Test
+    public void testFalloPrecisionDecimal() {
+    	float balance = cuenta.getBalance();
+    	for (int i = 0; i < 100; i++) balance += 0.23F;
+    	assertNotEquals(balance, 623F);
+    	assertEquals(balance, 622.99805F);
+    }
+
+}
