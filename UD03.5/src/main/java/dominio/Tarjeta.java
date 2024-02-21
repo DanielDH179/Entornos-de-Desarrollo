25c73963db985f3a9d7f19cb2bf468d8a632a5d4
DanielDH179 <95979922+DanielDH179@users.noreply.github.com>
2024-02-21 14:14:27 +0100

    Upload files

---
+++ UD03.5/src/main/java/dominio/Tarjeta.java
@@ -0,0 +1,26 @@
+package dominio;
+
+import java.util.Date;
+
+public abstract class Tarjeta {
+	
+	protected String mNumero, mTitular;
+	protected Date mFechaDeCaducidad;
+	protected Cuenta mCuentaAsociada;
+
+	public Tarjeta(String numero, String titular, Date fechaCaducidad) {
+		mNumero = numero;
+		mTitular = titular;
+		mFechaDeCaducidad = fechaCaducidad;
+	}
+	
+	public void setCuenta(Cuenta c) {
+		mCuentaAsociada = c;
+	}
+	
+	public abstract void retirar(double x) throws Exception;
+	public abstract void ingresar(double x) throws Exception;
+	public abstract void pagoEnEstablecimiento(String datos, double x) throws Exception;
+	public abstract double getSaldo();
+	
+}
---
+++ UD03.5/src/test/java/dominio/TestCuentaTarjeta.java
@@ -0,0 +1,23 @@
+package dominio;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.hamcrest.CoreMatchers.isA;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class TestCuentaTarjeta {
+
+}
