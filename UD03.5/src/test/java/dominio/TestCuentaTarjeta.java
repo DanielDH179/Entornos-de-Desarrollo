52f96ab45e0e661481ed9ee9037963a361b5bcd8
DanielDH179 <95979922+DanielDH179@users.noreply.github.com>
2024-02-21 14:41:04 +0100

    Update TestCuentaTarjeta.java

--- UD03.5/src/test/java/dominio/TestCuentaTarjeta.java
+++ UD03.5/src/test/java/dominio/TestCuentaTarjeta.java
@@ -13,11 +13,34 @@ import org.junit.jupiter.api.Test;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.hamcrest.CoreMatchers.isA;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TestCuentaTarjeta {
+	
+	Cuenta cuenta;
+	
+	@BeforeEach
+	void init() {
+		cuenta = new Cuenta("123456789", "Daniel");
+	}
+	
+	@AfterEach
+	void finish() {
+		cuenta = null;
+	}
+	
+	@ParameterizedTest
+	@ValueSource(doubles = {0D, 100D, -100D})
+	void testIngresarDinero() {
+		try {
+			cuenta.ingresar(100);
+		} catch (Exception ex) {
+			System.out.println("Ingreso de cantidad negativa");
+		}
+	}
 
 }
