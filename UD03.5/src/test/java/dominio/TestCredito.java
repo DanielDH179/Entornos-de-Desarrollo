4dba47893063ce1c56422ac62b73cf2333e6846d
DanielDH179 <95979922+DanielDH179@users.noreply.github.com>
2024-02-23 11:11:33 +0100

    Update TestCredito.java

--- UD03.5/src/test/java/dominio/TestCredito.java
+++ UD03.5/src/test/java/dominio/TestCredito.java
@@ -1,5 +1,25 @@
 package dominio;
 
+import java.util.Date;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+
 public class TestCredito {
+	
+	// Sesión de cobertura del 0% para Credito
+	Credito credito;
+	
+	@SuppressWarnings("deprecation")
+	@BeforeEach
+	void init() {
+		credito = new Credito("1", "Juan", new Date(2024, 1, 1), 200D);
+	}
+	
+	@AfterEach
+	void finish() {
+		credito = null;
+	}
 
 }
